9122627d8af751aad58de430c710d6ff5080734c
Yauheni Sharamed <SHaramed.EI@phystech.edu>
2018-11-15 00:13:00 +0300

    New realisation

--- pizza1.c
+++ pizza1.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define PILE_LEN 50
 
 void* sausager(void* p);
 void* saucer(void* p);
@@ -17,109 +18,100 @@ struct _monitor
 {
 	char pizza[4];
 	int num_pizza;
-	char need_to_check[50][4];
+	char need_to_check[PILE_LEN][4];
 	int num_to_check;
 	pthread_mutex_t busy;
-	pthread_cond_t new_pizza;
+	pthread_cond_t pizza_is_ready;
 	void (*put_elem)(struct _monitor* monitor , char c);
 	int (*check_pizza)(struct _monitor* monitor);
+	void (*pizza_to_check_pile)(struct _monitor* monitor);
 };
 
 
 int check_pizza(struct _monitor* monitor)
 {
-	int i;
+	pthread_mutex_lock(&(monitor->busy));
 	int ret = 0;
-	while(i < monitor->num_pizza)
-	{
-//		printf("checker\n");
-		pthread_mutex_lock(&(monitor->busy));
-		if(monitor->num_to_check > 0)
-		{
-			if((monitor->need_to_check[monitor->num_to_check][0]+
-			    monitor->need_to_check[monitor->num_to_check][1]+
-			    monitor->need_to_check[monitor->num_to_check][2]+
-			    monitor->need_to_check[monitor->num_to_check][3]) 
-			    != 'k' + 's' + 's')
-				 ret++;
-			i++;
-			monitor->num_to_check--;
-		}
-		else
-			pthread_cond_wait(&(monitor->new_pizza) , &(monitor->busy));
+	
+	if(monitor->num_to_check == 0)
+		pthread_cond_wait(&(monitor->pizza_is_ready) , &(monitor->busy));
 
-                pthread_mutex_unlock(&(monitor->busy));
-	}
+	if((monitor->need_to_check[monitor->num_to_check][0]+
+    	    monitor->need_to_check[monitor->num_to_check][1]+
+	    monitor->need_to_check[monitor->num_to_check][2]+
+	    monitor->need_to_check[monitor->num_to_check][3]) != ('k' + 's' + 's'))
+		ret++;
+	
+	monitor->num_to_check--;
+
+        pthread_mutex_unlock(&(monitor->busy));
 	return ret;
+}
+
+void pizza_to_check_pile(struct _monitor* monitor)
+{
+	monitor->num_to_check++;
+	int i = 0;
+	for(i = 0 ; i < 3 ; i++)
+	{
+        	monitor->need_to_check[monitor->num_to_check][i] = monitor->pizza[i];
+                monitor->pizza[i] = 0;
+	}
 }
 
 void put_elem(struct _monitor* monitor , char c)
 {
-int n , i;
-for(n = 0 ; n < monitor->num_pizza ; n++)
-{
-//	printf("%c before_critical_section\n" , c);
-	fflush(NULL);
 	pthread_mutex_lock(&(monitor->busy));
-//	printf("%c inside\n" , c);
-	fflush(NULL);	
 	
-	i = 0;	
+	int i = 0;	
 	while(0 != monitor->pizza[i])
 		i++;
 
-//	printf("-%d-" , i);	
 	monitor->pizza[i] = c;
 
 	if(i == 2)
 	{
-		monitor->num_to_check++;
-
-		monitor->need_to_check[monitor->num_to_check][0] = monitor->pizza[0];
-		monitor->need_to_check[monitor->num_to_check][1] = monitor->pizza[1];
-		monitor->need_to_check[monitor->num_to_check][2] = monitor->pizza[2];
-		monitor->need_to_check[monitor->num_to_check][3] = monitor->pizza[3];
-		
-//		printf("%s\n" , monitor->pizza);
-		fflush(NULL);
-		monitor->pizza[0] = 0;
-		monitor->pizza[1] = 0;
-		monitor->pizza[2] = 0;
-		monitor->pizza[3] = 0;
-
-		pthread_cond_broadcast(&(monitor->new_pizza));
-
-//		printf("signaled\n");
-		fflush(NULL);
+		monitor->pizza_to_check_pile(monitor);
+		pthread_cond_broadcast(&(monitor->pizza_is_ready));
 	}
 	else
 	{
-//              printf("%c wait\n" , c);
-                fflush(NULL);
-	//	sleep(1);
-                pthread_cond_wait(&(monitor->new_pizza) , &(monitor->busy));
-//              printf("get_sig\n");
-                fflush(NULL);
+                pthread_cond_wait(&(monitor->pizza_is_ready) , &(monitor->busy));
         }
 
 	pthread_mutex_unlock(&(monitor->busy));
 }
+
+
+void monitor_init(struct _monitor* monitor , int num_pizza)
+{
+	monitor->num_pizza = num_pizza;
+	monitor->put_elem = &put_elem;
+        monitor->check_pizza = &check_pizza;
+        monitor->pizza_to_check_pile = &pizza_to_check_pile;
+
+	monitor->num_to_check = 0;
+
+	pthread_mutex_init(&(monitor->busy) , NULL);
+	pthread_cond_init(&(monitor->pizza_is_ready) , NULL);
+
+        int i = 0;
+        for(i = 0 ; i < 3 ; i++)
+                monitor->pizza[i] = 0;
+
 }
-
-
 
 int main(int argc, char** argv)
 {
 	struct _monitor monitor;
 
 	char* l;
-        monitor.num_pizza = strtoll(argv[1] , &l , 10);
-	monitor.put_elem = &put_elem;
-	monitor.check_pizza = &check_pizza;
-	monitor.num_to_check = 0;
-	pthread_t id_sauce, id_sausage_1, id_sausage_2, id_checker;
+        int num_pizza = strtoll(argv[1] , &l , 10);
 
-	monitor.num_to_check = 0;
+
+	monitor_init(&monitor , num_pizza);
+
+	pthread_t id_sauce, id_sausage_1, id_sausage_2, id_checker;
 
 	pthread_mutex_init(&(monitor.busy) , NULL);
 
@@ -141,19 +133,27 @@ int main(int argc, char** argv)
 void* sausager(void* p)
 {
 	struct _monitor* monitor = (struct _monitor*) p;
-	monitor->put_elem(monitor , 'k');
+	int i = 0;
+	for(i = 0 ; i < monitor->num_pizza ; i++)
+		monitor->put_elem(monitor , 'k');
 }
 
 void* saucer(void* p)
 {
         struct _monitor* monitor = (struct _monitor*) p;
-        monitor->put_elem(monitor , 's');
+        int i = 0;
+	for(i = 0 ; i < monitor->num_pizza ; i++)
+		monitor->put_elem(monitor , 's');
 }
 
 void* checker(void* p)
 {
         struct _monitor* monitor = (struct _monitor*) p;
-        if(monitor->check_pizza(monitor))
+	int i = 0 , ans = 0;
+	for(i = 0 ; i < monitor->num_pizza ; i++)
+		ans += monitor->check_pizza(monitor);
+
+        if(ans)
 	{
 		printf("GOOD PIZZA\n");
 	}
@@ -163,5 +163,3 @@ void* checker(void* p)
 	}
 	fflush(NULL);
 }
-
-
