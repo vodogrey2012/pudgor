9feae6816bbc1ce92e09ebc61727f54b0da72561
Yauheni Sharamed <SHaramed.EI@phystech.edu>
2018-12-06 02:32:52 +0300

    Add sops init

--- ship.c
+++ ship.c
@@ -7,7 +7,8 @@
 
 
 
-#define SEMNUM 4
+#define SEMNUM 5
+#define SEMINSTR 15
 
 struct Val
 {
@@ -18,7 +19,7 @@ struct Val
 };
 
 
-int sem_init(struct sembuf* sops);
+int sem_init(struct sembuf* sops , struct Val val);
 struct Val* val_init(char* argv[]);
 void sops_init( struct sembuf* sops , unsigned short op_num , unsigned short sem_num ,       
                                                     short sem_op , short sem_flg);
@@ -36,8 +37,8 @@ int main(int argc , char* argv[])
     }
 
     struct Val* val = val_init( argv);
-    struct sembuf sops[SEMNUM];
-    int sem_id = sem_init( sops);
+    struct sembuf sops[SEMINSTR];
+    int sem_id = sem_init( sops , *val);
     
 
     return 0;
@@ -63,16 +64,34 @@ struct Val* val_init(char* argv[])
 }
 
 
-int sem_init(struct sembuf* sops)
+int sem_init(struct sembuf* sops , struct Val val)
 {
-    sops_init( sops , 1 , 1 , 3 , 0);
-
-
     int sem_id = semget(IPC_PRIVATE, SEMNUM, 0777|IPC_CREAT);
-    if(sem_id)
-        return sem_id;
+    if(sem_id <= 0)
+        exit(1);
 
-    exit(1);
+    sops_init( sops , 0 , 0 , -1, 0); // dicrease number of trips, for ship 
+    sops_init( sops , 1 , 0 , 0 , IPC_NOWAIT); // check available trip, for pass
+    sops_init( sops , 2 , 1 , -1 ,0); // pass go on ladder
+    sops_init( sops , 3 , 1 , 1 , 0); // pass go from  ladder 
+    sops_init( sops , 4 , 2 , 0 , 0); // pass check is ladder available
+    sops_init( sops , 5 , 2 , 1 , 0); // ship take up ladder
+    sops_init( sops , 6 , 2 , -1, 0); // ship take down ladder
+    sops_init( sops , 7 , 3 , -1, 0); // pass go from ship
+    sops_init( sops , 8 , 3 , val.ship_size , 0); // ship update data of pass removed
+    sops_init( sops , 9 , 4 , -1, 0); // new pass on ship
+    sops_init( sops , 10, 4 , val.ship_size , 0); // ship update data of new pass
+
+    //initial conditions
+    struct sembuf init[5]; 
+    sops_init( init , 0 , 0 , val.trip_num , 0);    // num of trips
+    sops_init( init , 1 , 1 , val.ladder_size , 0); // size of ladder
+    sops_init( init , 2 , 2 , 1 , 0);               // ladder is unavailable
+    sops_init( init , 3 , 3 , 0 , 0);               // nobody must go from ship
+    sops_init( init , 4 , 4 , val.ship_size , 0);   // set num of new pass
+    semop( sem_id , init , 5); 
+
+    return sem_id;
 }
 
 
